da111a819ed9fdfdd7a83cd12007223cdc704ca4
Ben Eisner <ben.a.eisner@gmail.com>
2024-03-18 23:03:54 -0400

    temporary low-dim state modifications. don't include these at the end

--- rlbench/tasks/reach_target.py
+++ rlbench/tasks/reach_target.py
@@ -43,9 +43,9 @@ class ReachTarget(Task):
     def base_rotation_bounds(self) -> Tuple[List[float], List[float]]:
         return [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]
 
-    def get_low_dim_state(self) -> np.ndarray:
-        # One of the few tasks that have a custom low_dim_state function.
-        return np.array(self.target.get_position())
+    # def get_low_dim_state(self) -> np.ndarray:
+    #     # One of the few tasks that have a custom low_dim_state function.
+    #     return np.array(self.target.get_position())
 
     def is_static_workspace(self) -> bool:
         return True
